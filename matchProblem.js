441645cb98606880dd8593c6cb7e504625889297
sazid0101 <shahriarislamsazid@gmail.com>
2025-01-21 17:02:03 +0600

    match problem solve

---
+++ matchProblem.js
@@ -0,0 +1,6 @@
+const sentence= "Bangladesh is a vibrant and culturally rich country in South Asia, bordered by India, Myanmar, and the Bay of Bengal. Known for its lush greenery and extensive waterways,bangladesh is home to the world's largest mangrove forest, the Sundarban, and the majestic Royal Bengal Tiger. With a history steeped in resilience, Bangladesh gained independence in 1971 and has since made remarkable progress in various sectors."
+
+const matches =sentence.match(/bangladesh/gi);
+const count=matches.length
+console.log(matches)
+console.log(count)
