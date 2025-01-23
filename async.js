155061ebb45dc0b29d13ca44bf7a5243aaf9ab45
sazid0101 <shahriarislamsazid@gmail.com>
2025-01-23 19:47:25 +0600

    async

---
+++ async.js
@@ -0,0 +1,12 @@
+console.log("print 1") 
+
+setTimeout(function(){
+    console.log("print 2")
+}, 2000);
+
+console.log("print 3")
+
+// setInterval(function(){
+//     console.log("print 4")
+// },2000);
+console.log("x")
