4cb74be9ece8d6a3901812695c5db6ac2d2f1353
sazid0101 <shahriarislamsazid@gmail.com>
2025-01-29 15:32:13 +0600

    ph problem solving assignment 4

---
+++ phProblemSolve4.js
@@ -0,0 +1,11 @@
+function password(obj){
+    
+        console.log(`${obj.siteName}#${obj.name}@${obj.birthYear}`)
+    
+}
+const passObj ={
+    name: "kolimudin",
+    birthYear: 1999,
+    siteName: "google"
+}
+password(passObj)
