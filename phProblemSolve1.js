ca6289329911ad04032099ef819e796c745d586f
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-02 12:35:42 +0600

    ..

--- phProblemSolve1.js
+++ phProblemSolve1.js
@@ -9,6 +9,39 @@ function calculateMoney( ticketSale){
     // console.log(totalCost , ticketSalePrice)
     return profit;
 }
+function checkName(name){
+    const lastLetter = name[name.length -1].toLowerCase() ;
+    
+    if(typeof name !== "string"){
+        return "Invalid"
+    }
+    else if(lastLetter === "a" || lastLetter === "y" || lastLetter === "e" || lastLetter === "o" || lastLetter === "u" || lastLetter === "w"){
+        return "Good Name" ;
+    }
+    else{
+        return "Bad Name"
+    }
+}
+function deleteInvalids(array){
+    
+    if(!Array.isArray(array)){
+        return "Invalid Array"
+    }
+
+    const numArray =[];
+    for(const arrayElement of array){
+         if(typeof arrayElement === "number" && !Number.isNaN(arrayElement)){
+            numArray.push(arrayElement)
+        }
+    }
+    return numArray;
+}
+function password(obj){
+    
+    console.log(`${obj.siteName}#${obj.name}@${obj.birthYear}`)
+
+} 
+
 console.log(calculateMoney(10)) ;
 console.log(calculateMoney(1055)) ;
 console.log(calculateMoney(93)) ;
