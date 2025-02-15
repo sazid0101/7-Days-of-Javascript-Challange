ba000102a62369743099fb08dae2c62435c7c7ce
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-15 15:43:43 +0600

    map varient

--- map.js
+++ map.js
@@ -1,9 +1,23 @@
+//map
+
+// const myArray = [4,3,2,7,9,3,6]
+
+// function doubleIt(num){
+//     console.log(num)
+//     return num*3
+// }
+
+// const result = myArray.map(doubleIt)
+// console.log(result)
+
+
+// map another
 const myArray = [4,3,2,7,9,3,6]
-
-function doubleIt(num){
-    console.log(num)
-    return num*3
-}
-
-const result = myArray.map(doubleIt)
+const double = n => n*2
+const result = myArray.map(double)
 console.log(result)
+
+//map another 
+const array1 = [1,4,7,9,2]
+const double1 = array1.map(n=>n*2)
+console.log(double1)
