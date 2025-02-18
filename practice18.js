cec7c2b3cc76b227cc756e8f48acde96b187d6dd
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-18 13:15:13 +0600

    practice

--- practice18.js
+++ practice18.js
@@ -4,3 +4,16 @@ let q=p;
 p='React'; 
 console.log(q);
 console.log(p)
+
+
+
+const isTrue=true;
+console.log(!isTrue ? "hello" : "world")
+console.log(!isTrue)
+
+
+const sum =(p, q)=> {
+    p + q;
+   }
+   const result = sum(2, 3);
+   console.log(result);
