6e1a5154a78be033a6ba3736f17cf680cc1c7f4d
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-18 15:57:39 +0600

    json parse

---
+++ json2.js
@@ -0,0 +1,3 @@
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then(Response => Response.json())
+.then(json => console.log(json))
--- practice18.js
+++ practice18.js
@@ -17,3 +17,17 @@ const sum =(p, q)=> {
    }
    const result = sum(2, 3);
    console.log(result);
+
+
+   if ("2" === 2) {
+    console.log("Inside if");
+   } else {
+    console.log("Inside else");
+   }
+
+
+
+   function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32));
