29d3d2973a5c7e250b7d901446cb55d645061b0c
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-26 11:30:31 +0600

    ..

---
+++ overview.js
@@ -0,0 +1,11 @@
+// function multiply(n1,n2){
+//     result = n1 * n2
+//     return result
+//   }
+//   const output = multiply(5,6)
+//   console.log(output)
+
+
+const isEven = n => n%2 == 0;
+const result = isEven(91)
+console.log(result)
