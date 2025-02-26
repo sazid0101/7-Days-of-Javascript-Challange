1765abc1d9caf0bb55e88be0994db763e67db7b5
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-26 11:17:31 +0600

    ..

--- functionCall.js
+++ functionCall.js
@@ -1,17 +1,24 @@
-const person = {
-    fullName: function(city, country) {
-      return this.firstName + " " + this.lastName + " lives in " + city + ", " + country;
-    }
-  }
-  const person1 = {
-    firstName:"sazid",
-    lastName: "islam"
-  }
-  const person2 = {
-    firstName:"shahriar",
-    lastName: "islam"
-  }
+// const person = {
+//     fullName: function(city, country) {
+//       return this.firstName + " " + this.lastName + " lives in " + city + ", " + country;
+//     }
+//   }
+//   const person1 = {
+//     firstName:"sazid",
+//     lastName: "islam"
+//   }
+//   const person2 = {
+//     firstName:"shahriar",
+//     lastName: "islam"
+//   }
 
-  console.log(person.fullName.call(person1, "Dhaka", "Bangladesh")); //call()
+//   console.log(person.fullName.call(person1, "Dhaka", "Bangladesh")); //call()
 
-  console.log(person.fullName.apply(person2, ["chittagong", "Bangladesh"]));  //apply()
+//   console.log(person.fullName.apply(person2, ["chittagong", "Bangladesh"]));  //apply()
+
+function multiply(n1,n2){
+  result = n1 * n2
+  return result
+}
+const output = multiply(5,6)
+console.log(output)
--- json2.js
+++ json2.js
@@ -1,3 +1,8 @@
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then(Response => Response.json())
+// .then(json => console.log(json))
+
+
 fetch("https://jsonplaceholder.typicode.com/todos/1")
-.then(Response => Response.json())
-.then(json => console.log(json))
+.then(Response => console.log(Response))
+// .then(json => console.log(json))
